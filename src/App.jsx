57cda07c120cdedca657152e49fb40b72b0adffd
Nusratullokh <nusratullokh7700@gmail.com>
2024-06-13 18:13:30 +0500

    finished

---
+++ src/App.jsx
@@ -0,0 +1,340 @@
+import './App.css';
+import avatar from "./image/Avatar.svg" 
+import Icon from "./image/Icon.svg" 
+import padarok from "./image/padarok.svg" 
+import qongro from "./image/qongro.svg" 
+import girl from "./image/girl.svg"
+import soat from "./image/soat.png"
+import kalonka from "./image/kalonka.png"
+import navushnik from "./image/kalonka.png"
+import covid from "./image/covid.png"
+import chexol from "./image/chexol.png"
+import notebook from "./image/notebook.png"
+import garri from "./image/garri.svg"
+import kareyanka from "./image/kareyanka.svg"
+import oqsoch from "./image/oq soch.svg"
+import blondinka from "./image/blondinka.svg"
+import klawish from "./image/klawish.png"
+import tufli from "./image/tufli.png"
+import salatchi from "./image/salatchi.png"
+import retro from "./image/retro.png"
+import tuya from "./image/tuya.png"
+import dengiz from "./image/dengiz.png"
+import Iconn from "./image/Icon Color.svg"
+import nuqta from "./image/nuqta.svg"
+import facebook from "./image/facebook.svg"
+import instagram from "./image/instagram.svg"
+import twitter from "./image/twitter.svg"
+import be from "./image/be.svg"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function App() {
+  return (
+<div className="app">
+<nav>
+      <div className="container">
+        <div className="nav__wrapper">
+          <h2>Fabrx</h2>
+            <div className='nav__icons'> 
+             <div className='nav__avatar'>
+               <img src={avatar} alt="" />
+                <p>Bruce Lee</p>
+                <img src={Icon} alt="" />
+             </div>
+             <div className='nav__padarok'>
+              <img src={padarok} alt="" />
+              <img src={qongro} alt="" />
+              </div>
+            </div>
+         </div>
+      </div>
+   </nav>
+
+   <section className="hero">
+    <div className="container">
+      <div className="hero__wrapper">
+        <div className="hero__content">
+          <h2>5 mind blowing facts about the iPhone 14</h2>
+          <div className="hero__icon">
+            <img src={girl} alt="" />
+            <small>Frok Rastegar   . 1 hour ago</small>
+          </div>
+        </div>
+      </div>
+    </div>
+   </section>
+
+  
+     <section className="articl">
+     <div className="container">
+        <div className="articl__title">
+          <h2>Featured articles</h2>
+        </div>
+        <div className="articl__wrapper">
+          <div className="articl__cards">
+            <div className="articl__card">
+                <img src={soat} alt="" />
+                <div className="articl__card-content">
+                  <h3>10 beaches you must visit</h3>
+                  <div className="articl__icon">
+                     <img src={garri} alt="" />
+                     <small>Farok Rastegar   . 1 hour ago</small>
+                  </div>
+                  <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the…</p>
+                </div>
+            </div>
+            <div className="articl__card">
+              <img src={kalonka} alt="" />
+              <div className="articl__card-content">
+                  <h3>10 beaches you must visit</h3>
+                  <div className="articl__icon">
+                     <img src={girl} alt="" />
+                     <small>Farok Rastegar   . 1 hour ago</small>
+                  </div>
+                  <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the…</p>
+                </div>
+            </div>
+            <div className="articl__card">
+              <img src={navushnik} alt="" />
+              <div className="articl__card-content">
+                  <h3>10 beaches you must visit</h3>
+                  <div className="articl__icon">
+                     <img src={garri} alt="" />
+                     <small>Farok Rastegar   . 1 hour ago</small>
+                  </div>
+                  <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the…</p>
+                </div>
+            </div>
+            <div className="articl__card">
+              <img src={covid} alt="" />
+              <div className="articl__card-content">
+                  <h3>10 beaches you must visit</h3>
+                  <div className="articl__icon">
+                     <img src={kareyanka} alt="" />
+                     <small>Farok Rastegar   . 1 hour ago</small>
+                  </div>
+                  <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the…</p>
+                </div>
+            </div>
+            <div className="articl__card">
+            <img src={chexol} alt="" />
+            <div className="articl__card-content">
+                  <h3>10 beaches you must visit</h3>
+                  <div className="articl__icon">
+                     <img src={oqsoch} alt="" />
+                     <small>Farok Rastegar   . 1 hour ago</small>
+                  </div>
+                  <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the…</p>
+                </div>
+            </div>
+            <div className="articl__card">
+              <img src={notebook} alt="" />
+              <div className="articl__card-content">
+                  <h3>10 beaches you must visit</h3>
+                  <div className="articl__icon">
+                     <img src={blondinka} alt="" />
+                     <small>Farok Rastegar   . 1 hour ago</small>
+                  </div>
+                  <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the…</p>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+     </section>
+
+
+<section className='featured'>
+<div className="container">
+  <div className="featured__wrapper">
+    <div className="featured__content">
+      <a href="#">Featured</a>
+      <h2>Beached to take you somewhere else</h2>
+      <div className="featured__icon">
+            <img src={girl} alt="" />
+            <small>Frok Rastegar   . 1 hour ago</small>
+          </div>
+    </div>
+    <div className="featured__img">
+      <img src={klawish} alt="" />
+    </div>
+  </div>
+</div>
+</section>
+
+<section className='group'>
+<div className="container">
+  <div className="group__title">
+  <h2>Featured articles</h2>
+  </div>
+  <div className="group__cards">
+    <div className="group__card">
+      <img src={tufli} alt="" />
+      <div className="group__card-content">
+        <h2>Pathway to the Mediterranean</h2>
+        <div className="group__icon">
+            <img src={girl} alt="" />
+            <small>Frok Rastegar   . 1 hour ago</small>
+          </div>
+          <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain a…</p>
+      </div>
+    </div>
+    <div className="group__card">
+      <img src={salatchi} alt="" />
+      <div className="group__card-content">
+      <h2>My awesome safari in Lahbab Desert, United Arab Emirates</h2>
+        <div className="group__icon">
+            <img src={girl} alt="" />
+            <small>Frok Rastegar   . 1 hour ago</small>
+          </div>
+          <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain a…</p>
+      </div>
+   
+    </div>
+
+    <div className="group__card">
+      <img src={retro} alt="" />
+      <div className="group__card-content">
+      <h2>10 amazing things to do in Cuba</h2>
+        <div className="group__icon">
+            <img src={girl} alt="" />
+            <small>Frok Rastegar   . 1 hour ago</small>
+          </div>
+          <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain a…</p>
+      </div>
+     
+    </div>
+
+    <div className="group__card">
+      <img src={tuya} alt="" />
+      <div className="group__card-content">
+      <h2>The best street food in the world. Ranked!</h2>
+        <div className="group__icon">
+            <img src={girl} alt="" />
+            <small>Frok Rastegar   . 1 hour ago</small>
+          </div>
+          <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain a…</p>
+      </div>
+      
+    </div>
+
+    <div className="group__card">
+      <img src={dengiz} alt="" />
+      <div className="group__card-content">
+      <h2>Travel Packing guide for beginners</h2>
+        <div className="group__icon">
+            <img src={girl} alt="" />
+            <small>Frok Rastegar   . 1 hour ago</small>
+          </div>
+          <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain a…</p>
+      </div>
+      
+    </div>
+
+
+  </div>
+  <div className="group__link">
+  <a  href="">
+    Load more
+  </a>
+  </div>
+ 
+   </div>
+</section>
+
+<section className='inbox'>
+  <div className="container">
+  <div className="inbox__wrapper">
+  <div className="inbox__content">
+    <h2>  Incredible deals, right to your inbox!</h2>
+    <div className='inbox__btn'>
+    <input type="text " placeholder='Placeholder'  /> <img src={Iconn} alt="" />
+    </div  >
+    <span><a href="">By joining you agree to our Terms and Conditions</a></span>
+  </div>
+  </div>
+  </div>
+</section>
+
+<div className="heder">
+  <div className="container">
+  <div className="heder__wrapper">
+  <div className="heder__content">
+    <ul className='heder__title'>
+      <li><a className='title__link' href="">First Column</a></li>
+      <li><a className='heder__link' href="">Link One</a></li>
+      <li><a className='heder__link' href="">Link Two</a></li>
+      <li><a className='heder__link' href="">Link Three</a></li>
+      <li><a className='heder__link' href="">Link Four</a></li>
+
+    </ul>
+    <ul className='heder__title'>
+      <li><a className='title__link' href="">First Column</a></li>
+      <li><a className='heder__link' href="">Link One</a></li>
+      <li><a className='heder__link' href="">Link Two</a></li>
+      <li><a className='heder__link' href="">Link Three</a></li>
+      <li><a className='heder__link' href="">Link Four</a></li>
+
+    </ul>
+    <ul className='heder__title'>
+      <li><a className='title__link' href="">First Column</a></li>
+      <li><a className='heder__link' href="">Link One</a></li>
+      <li><a className='heder__link' href="">Link Two</a></li>
+      <li><a className='heder__link' href="">Link Three</a></li>
+      <li><a className='heder__link' href="">Link Four</a></li>
+
+    </ul>
+    <ul className='heder__title'>
+      <li><a className='title__link' href="">First Column</a></li>
+      <li><a className='heder__link' href="">Link One</a></li>
+      <li><a className='heder__link' href="">Link Two</a></li>
+      <li><a className='heder__link' href="">Link Three</a></li>
+      <li><a className='heder__link' href="">Link Four</a></li>
+
+    </ul>
+    
+  </div>
+  <div className="heder__input">
+    <p>Never miss an update</p>
+    <div className='heder__btn'>
+    <input type="text " placeholder='Placeholder'  /> <img src={Iconn} alt="" />
+    </div  >
+  </div>
+  </div>
+  <div className='heder__icon'>
+    <img src={nuqta} alt="" />
+    <a href="">© 2019 Fabrx </a>
+    <div className='icons'>
+      <a href="https://www.facebook.com/"><img src={facebook} alt="" /></a>
+      <a href="https://www.instagram.com/accounts/login/?hl=en"><img src={instagram} alt="" /></a>
+      <a href="https://x.com/i/flow/login"><img src={twitter} alt="" /></a>
+      <a href=""><img src={be} alt="" /></a>
+    </div>
+  </div>
+  </div>
+</div>
+
+
+</div>
+  
+   
+  );
+}
+
+export default App;
